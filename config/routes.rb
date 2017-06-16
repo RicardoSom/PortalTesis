28db7ace40f9ed52fedb3242902c0dbb4484f006
Ricardo Orellana Munoz <r.orellana.munoz@gmail.com>
2017-06-16 18:30:46 -0400

    Creacion de las rutas del modelo Document

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :documents
   resources :schools
   resources :users
   resources :careers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'documents#index'
 end
