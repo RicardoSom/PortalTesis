9545d4fcb602fcb913bab26aa564ffc3a37ef733
Ricardo Orellana Munoz <r.orellana.munoz@gmail.com>
2017-06-20 17:54:25 -0400

    Integracion de devise y reemplazo de algunos metodos user por los metodos que entrega la gema devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :documents
   resources :schools
-  resources :users
+  #resources :users
   resources :careers
+  get 'users' => 'users#index'
+  get 'user' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'documents#index'
 end
